fd04dbf9380fd54972369bf073c170dfc4081c14
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2020-11-13 19:48:17 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// LIBRERIAS
+const express = require("express");
+
+// SE INSTANCIA EL APP DE EXPRESS
+const app = express();
+
+// SE INSTANCIA EL PUERTO
+const port = process.env.PORT || 4000;
+
+
+//creamos las rutas del api
+app.use("/api/problemas", require("./routes/problemas"));
+
+// SE CREA EL SERVICIO
+app.listen(port, '0.0.0.0', () => {
+    console.log("Running on", port);
+  });
