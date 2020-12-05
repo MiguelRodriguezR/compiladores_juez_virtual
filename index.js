4ebfedad17118bfbfb0f3f90156eabd2762b602f
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2020-12-04 19:54:55 -0500

    changes test sh

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 4000;
 
 //creamos las rutas del api
 app.use("/api/problemas", require("./routes/problemas"));
+app.use("/exec/test", require("./routes/test"));
 
 // SE CREA EL SERVICIO
 app.listen(port, '0.0.0.0', () => {
     console.log("Running on", port);
-  });
+});
---
+++ routes/test.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const exec = require("child_process").exec;
+
+router.get("/", (req, res) => {
+  console.log("Request received.");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  //executes my shell script - main.sh when a request is posted to the server
+  exec("sh main.sh", function (err, stdout, stderr) {
+    if (err) console.log(err);
+
+    //Print stdout/stderr to console
+    console.log('out', stdout);
+    console.log('error', stderr);
+
+    //Simple response to user whenever localhost:8888 is accessed
+    res.send(stdout);
+  });
+});
+
+module.exports = router;
