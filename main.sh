48c1d6e3996662d50fbb003e90677402626aa6e3
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2020-12-16 22:07:14 -0500

    controllers

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ echo "#include <iostream>
 using namespace std;
 int main()
 {
-    std::cout << "Hola Mundo!";
+    std::cout <<  \"Hola Mundo!\";
     return 1;
 }" > hola.cpp
 g++ -o ejec hola.cpp 
