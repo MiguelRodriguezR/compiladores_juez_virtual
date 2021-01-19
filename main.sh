c2e05df6bdb486f9b860b0998bec2be6750eb774
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-18 21:08:51 -0500

    test

--- main.sh
+++ main.sh
@@ -6,10 +6,9 @@ using namespace std;
 int main(int argc, const char*argv[])
 {
     float a,b,c;
-    a=std::stof(argv[1]);
-    b=std::stof(argv[2]);
-    c=a+b;
-    cout<<a<<\"+\"<<b<<\"=\"<<c<<endl;
+    a=argv[1];
+    b=argv[2];
+    cout<<a<<\"+\"<<b<<\"=\"<<endl;
     return 0;
 }" > test.cpp
 g++ test.cpp -o testexe
