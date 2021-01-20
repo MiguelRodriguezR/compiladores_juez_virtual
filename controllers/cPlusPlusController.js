88da8ba9a1b32fa583ac5955875c5f6ea8b73735
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-20 18:07:52 -0500

    database changes

--- controllers/cPlusPlusController.js
+++ controllers/cPlusPlusController.js
@@ -3,29 +3,14 @@ const { request } = require("express");
 const router = require("../routes/cplusplus");
 const shBuilder = require("../helpers/shBuilder");
 const fs = require("fs");
+const axios = require('axios');
 
 exports.compileFile = async (req, res) => {
   try {
     const nameHash = "testerFile";
     const idProblema = req.body.idProblem;
-    const casos = [
-      {
-        parametros: [2, 2],
-        respuestaEsperada: 4,
-      },
-      {
-        parametros: [1, 2],
-        respuestaEsperada: 3,
-      },
-      {
-        parametros: [5, 5],
-        respuestaEsperada: 10,
-      },
-      {
-        parametros: [25, 50],
-        respuestaEsperada: 75,
-      },
-    ];
+    const response = await axios.get(`localhost:3000/problems/${idProblema}`);
+    const casos = response.casos;
     const respuestasCasos = [];
     await Promise.all(
       casos.map(async (caso, i) => {
