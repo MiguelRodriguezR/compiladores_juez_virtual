71c681d5532abb9e5129aefeda7f9c3de724ef5c
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-19 19:27:29 -0500

    changes functional data

--- controllers/cPlusPlusController.js
+++ controllers/cPlusPlusController.js
@@ -5,47 +5,51 @@ const shBuilder = require("../helpers/shBuilder");
 const fs = require("fs");
 
 exports.compileFile = async (req, res) => {
-  const nameHash = "testerFile";
-  const idProblema = req.body.idProblem;
-  const casos = [
-    {
-      parametros: [1, 2],
-      respuestaEsperada: 3,
-    },
-    {
-      parametros: [25, 50],
-      respuestaEsperada: 75,
-    },
-  ];
-  const respuestasCasos = [];
-  await Promise.all(
-    casos.map(async (caso, i) => {
-      const name = `${nameHash}${i}`;
-      shBuilder.createShFile(name, req.body.code, caso.parametros, () => {
-        exec(`sh ${name}.sh`, function (err, stdout, stderr) {
-          if (stdout) {
-            // se envia el codigo 200 todo esta bien al compilar
-            if (stdout == caso.respuestaEsperada) {
-              respuestasCasos.push(`test ${i} Pass`);
-            } else {
-              respuestasCasos.push(
-                `Se esperaba: "${caso.respuestaEsperada}" en vez de: "${stdout}"`
-              );
+  try {
+    const nameHash = "testerFile";
+    const idProblema = req.body.idProblem;
+    const casos = [
+      {
+        parametros: [1, 2],
+        respuestaEsperada: 3,
+      },
+      {
+        parametros: [25, 50],
+        respuestaEsperada: 75,
+      },
+    ];
+    const respuestasCasos = [];
+    await Promise.all(
+      casos.map(async (caso, i) => {
+        const name = `${nameHash}${i}`;
+        shBuilder.createShFile(name, req.body.code, caso.parametros, () => {
+          exec(`sh ${name}.sh`, function (err, stdout, stderr) {
+            if (stdout) {
+              // se envia el codigo 200 todo esta bien al compilar
+              if (stdout == caso.respuestaEsperada) {
+                respuestasCasos.push(`test ${i} Pass`);
+              } else {
+                respuestasCasos.push(
+                  `Se esperaba: "${caso.respuestaEsperada}" en vez de: "${stdout}"`
+                );
+              }
+            } else if (stderr) {
+              // se enia el codigo 400 hay un error al compilar
+              // res.status(400).json({ msg: stderr });
             }
-          } else if (stderr) {
-            // se enia el codigo 400 hay un error al compilar
-            // res.status(400).json({ msg: stderr });
-          }
-          console.log(respuestasCasos);
+            console.log(respuestasCasos);
+          });
         });
-      });
-      try {
-        fs.unlinkSync(`./${name}.sh`);
-        //file removed
-      } catch (err) {
-        console.error(err);
-      }
-    })
-  );
-  res.send(respuestasCasos);
+        try {
+          fs.unlinkSync(`./${name}.sh`);
+          //file removed
+        } catch (err) {
+          console.error(err);
+        }
+      })
+    );
+    res.json(respuestasCasos);
+  } catch (error) {
+    console.log(error);
+  }
 };
