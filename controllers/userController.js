fd68cfa288edc931d08a5e568a679b9338e494d6
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-02-13 21:12:36 -0500

    changes services

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,20 @@
 const axios = require("axios");
+const { request } = require("express");
+
 
 exports.getUser = async (req, res) => {
   const response = await axios.get(`http://localhost:3000/users`);
   res.send(response.data);
+};
+
+exports.addUserProblem = async (req, res) => {
+  idUser = req.body.idUser;
+  idProblem = req.body.idProblem;
+  const responseUser = await axios.get(`http://localhost:3000/users/${idUser}`);
+  const user = responseUser.data;
+  if(user && user.solvedProblems && !user.solvedProblems.find( p => p == idProblem)){
+    user.solvedProblems = [...user.solvedProblems, idProblem]
+    await axios.put(`http://localhost:3000/users/${idUser}`, user);
+  }
+  res.send(user);
 };
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 
 router.get('/', userController.getUser);
+router.put('/', userController.addUserProblem);
 
 module.exports = router;
